8fa5fbf8913e3c9a22392a66bb49bae56541a239
Harsh Negi <harshnegi0927@gmail.com>
2026-06-02 16:29:07 +0530

    Docker done

--- app/database.py
+++ app/database.py
@@ -1,35 +1,38 @@
-from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, DateTime, JSON
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, DateTime, JSON, text
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./store_intelligence.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}  
-)
+# SQLite aur PostgreSQL dono handle karo
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# ─── Table Definition ───────────────────────────────────────
 class EventModel(Base):
     __tablename__ = "events"
 
-    event_id    = Column(String, primary_key=True) 
-    store_id    = Column(String, index=True)        
-    camera_id   = Column(String)
-    visitor_id  = Column(String, index=True)         
-    event_type  = Column(String, index=True)
-    timestamp   = Column(DateTime, index=True)
-    zone_id     = Column(String, nullable=True)
-    dwell_ms    = Column(Integer, default=0)
-    is_staff    = Column(Boolean, default=False)
-    confidence  = Column(Float)
-    event_metadata = Column(JSON)                   
+    event_id       = Column(String, primary_key=True)
+    store_id       = Column(String, index=True)
+    camera_id      = Column(String)
+    visitor_id     = Column(String, index=True)
+    event_type     = Column(String, index=True)
+    timestamp      = Column(DateTime, index=True)
+    zone_id        = Column(String, nullable=True)
+    dwell_ms       = Column(Integer, default=0)
+    is_staff       = Column(Boolean, default=False)
+    confidence     = Column(Float)
+    event_metadata = Column(JSON)
 
 def get_db():
     db = SessionLocal()
